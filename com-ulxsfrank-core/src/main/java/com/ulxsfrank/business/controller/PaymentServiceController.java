a2c364c9ca834045bb97e3d14fea30d4e5416424
frankdevhub <frankdevhub@gmail.com>
2019-10-02 20:04:53 +0800

    wechat payment service

--- com-ulxsfrank-core/src/main/java/com/ulxsfrank/business/controller/PaymentServiceController.java
+++ com-ulxsfrank-core/src/main/java/com/ulxsfrank/business/controller/PaymentServiceController.java
@@ -10,17 +10,22 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.github.wxpay.sdk.WXPayUtil;
+import com.ulxsfrank.business.configuration.SnowFlakeGenerator;
+import com.ulxsfrank.business.configuration.SnowFlakeIdWorker;
 import com.ulxsfrank.business.data.Constants;
 import com.ulxsfrank.business.data.logging.Logger;
 import com.ulxsfrank.business.data.logging.LoggerFactory;
@@ -42,25 +47,55 @@ public class PaymentServiceController {
 
 	private Logger LOGGER = LoggerFactory.getLogger(PaymentServiceController.class);
 
-	private String getSignature(String openId) throws Exception {
+	private Map<String, String> getSignature(Map<String, String> map) throws Exception {
+		LOGGER.begin().headerAction(MessageMethod.EVENT).info("start do getSignature.");
+
+		String openId = map.get("openid");
+
 		String wxNonceStr = WXPayUtil.generateNonceStr();
-		System.out.println(wxNonceStr);
+		String tradeNumber = Long.toString(new SnowFlakeIdWorker().nextId());
+		System.out.println(String.format("using nonce string:[%s]", wxNonceStr));
 		Map<String, String> paraMap = new HashMap<String, String>();
 		paraMap.put("appid", Constants.WX_APP_ID);
+		paraMap.put("openid", openId);
 		paraMap.put("body", "高级服装设计订单");
 		paraMap.put("mch_id", Constants.WX_MCH_ID);
 		paraMap.put("nonce_str", wxNonceStr);
-		paraMap.put("openid", openId);
-		paraMap.put("out_trade_no", "3438118099812");
+		paraMap.put("out_trade_no", tradeNumber);
 		paraMap.put("spbill_create_ip", "58.34.44.32");
+		paraMap.put("total_fee", "1");
 
 		paraMap.put("trade_type", "JSAPI");
 		paraMap.put("notify_url", "www.jilu-samplestudio.com/payment/callback");
 		String sign = WXPayUtil.generateSignature(paraMap, Constants.WX_PATERNER_KEY);
+		System.out.println(String.format("generate sign as:[%s]", sign));
+
 		paraMap.put("sign", sign);
+		System.out.println(WXPayUtil.mapToXml(paraMap));
+		return paraMap;
+	}
+
+	private Map<String, String> postOrderRequest(Map<String, String> paraMap) throws Exception {
+
+		LOGGER.begin().headerAction(MessageMethod.EVENT).info("start do postOrderRequest.");
+
 		String xml = WXPayUtil.mapToXml(paraMap);
-		System.out.println(xml);
-		return null;
+		StringEntity bodyEntity = new StringEntity(xml);
+		String unifiedorderUrl = "https://api.mch.weixin.qq.com/pay/unifiedorder";
+
+		CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+		HttpPost httpPost = new HttpPost(unifiedorderUrl);
+		RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(5000).setConnectionRequestTimeout(5000)
+				.setSocketTimeout(5000).setRedirectsEnabled(true).build();
+		httpPost.setConfig(requestConfig);
+		httpPost.setEntity(bodyEntity);
+
+		CloseableHttpResponse response = httpClient.execute(httpPost);
+		HttpEntity responseEntity = response.getEntity();
+		String responseText = EntityUtils.toString(responseEntity);
+		System.out.println(String.format("response[pay_order_request]:[%s]", responseText));
+
+		return paraMap;
 	}
 
 	private Map<String, String> getWxAccessToken(String code)
@@ -87,6 +122,7 @@ public class PaymentServiceController {
 		Map<String, String> resMap = new HashMap<String, String>();
 		resMap.put("access_token", null);
 		resMap.put("openid", null);
+		resMap.put("code", code);
 
 		RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(5000).setConnectionRequestTimeout(5000)
 				.setSocketTimeout(5000).setRedirectsEnabled(true).build();
@@ -95,10 +131,10 @@ public class PaymentServiceController {
 
 		HttpEntity responseEntity = response.getEntity();
 		String responseText = EntityUtils.toString(responseEntity);
-		System.out.println(String.format("response:[%s]", responseText));
+		System.out.println(String.format("response[get_access_token]:[%s]", responseText));
 
 		JSONObject responseObject = JSON.parseObject(responseText);
-		if ((null != responseObject.get("access_token")) && (null != responseObject.get("openid"))) {
+		if ((null != responseObject.getString("access_token")) && (null != responseObject.getString("openid"))) {
 			resMap.put("access_token", responseObject.getString("access_token").toString());
 			resMap.put("openid", responseObject.getString("openid").toString());
 		} else {
@@ -109,22 +145,20 @@ public class PaymentServiceController {
 		return resMap;
 	}
 
-	@RequestMapping(value = "/order", method = RequestMethod.GET)
-	public Response<Map<String, String>> prePayService(@RequestBody String code) {
+	@RequestMapping(value = "/order", method = RequestMethod.POST)
+	public Response<Map<String, String>> prePayService(@RequestParam(name = "code") String code) {
 		try {
 			LOGGER.begin().headerAction(MessageMethod.GET).info("start prepay service.");
-			Map<String, String> resMap = getWxAccessToken(code);
-			String accessToken = resMap.get("access_token");
-			String openId = resMap.get("openid");
-			return new Response<Map<String, String>>().setData(null).success();
+
+			Map<String, String> tokenResponseMap = getWxAccessToken(code);
+			Map<String, String> signResponseMap = getSignature(tokenResponseMap);
+			// Map<String, String> orderResponseMap =
+			// postOrderRequest(signResponseMap);
+
+			return new Response<Map<String, String>>().setData(null).setMessage("success").success();
 		} catch (Exception e) {
-			return new Response<Map<String, String>>().setData(null).failed();
+			return new Response<Map<String, String>>().setData(null).setMessage(e.getMessage()).failed();
 		}
-	}
-
-	public static void main(String[] args) throws Exception {
-		new PaymentServiceController().getWxAccessToken("081Tslit1s3aSf0utwht1fpiit1Tsli6");
-
 	}
 
 }
